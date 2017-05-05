0f99873e90a7a0234c470ba8b84ae587bd94eb5d
Bryan Holman <bryanpholman@gmail.com>
2017-05-05 09:43:44 -0400

    Historical data is now shown along with forecast data. Closes #4.

--- R/analyze_irl_setup.R
+++ R/analyze_irl_setup.R
@@ -5,37 +5,77 @@
 
 library(reshape2)
 library(ggplot2)
+library(lubridate)
+
+# functions ---------------------------------------------------------------
+
+# given a data frame of ensemble data, add ens average, max, min, and median
+addEnsStats <- function(df.ens, cols = 2:22) {
+    df.ens$avg <- rowMeans(df.ens[cols], na.rm = FALSE)
+    df.ens$min <- apply(df.ens[cols], 1, min)
+    df.ens$max <- apply(df.ens[cols], 1, max)
+    df.ens$med <- apply(df.ens[cols], 1, median)
+    return(df.ens)
+}
+
 
 # df.setup.melt <- melt(df.setup, id.vars = 'validtime')
 
 # # the code below does okay ... not awesome ... too many lines!
 # p <- ggplot(df.setup.melt, aes(x = validtime, y = value, color = variable)) + 
 #     geom_line() + theme_minimal() + theme(legend.position="bottom")
-load('~/Dropbox/IRLSetup/data/df_setup.RData')
-df.test <- df.setup[4:28,]
-df.setup.melt <- melt(df.test, id.vars = 'validtime')
-df.test$avg.raw <- rowMeans(df.test[-1], na.rm = FALSE)
-df.test$min.raw <- apply(df.test[-c(1, 23)], 1, min)
-df.test$max.raw <- apply(df.test[-c(1, 23)], 1, max)
-df.test$med.raw <- apply(df.test[-c(1, 23)], 1, median)
-p <- ggplot(df.test, aes(x = validtime)) + 
-    geom_line(data = df.setup.melt, mapping = aes(x = validtime, y = value, 
-                                                  color = variable)) + 
+load('~/Dropbox/IRLSetup/data/setup.RData')
+# remove the missing rows in gefs.setup
+gefs.setup.1 <- gefs.setup.1[4:28,]
+gefs.setup.2 <- gefs.setup.2[4:28,]
+gefs.setup.3 <- gefs.setup.3[4:28,]
+gefs.setup.recent <- gefs.setup.recent[4:28,]
+# match asos times
+asos.setup <- asos.setup[asos.setup$roundvalid >= gefs.setup.1$validtime[1],]
+gefs.setup.recent.melt <- melt(gefs.setup.recent, id.vars = 'validtime')
+gefs.setup.1 <- addEnsStats(gefs.setup.1)
+gefs.setup.2 <- addEnsStats(gefs.setup.2)
+gefs.setup.3 <- addEnsStats(gefs.setup.3)
+gefs.setup.recent <- addEnsStats(gefs.setup.recent)
+date.breaks <- seq.POSIXt(asos.setup$roundvalid[1] - hours(3), 
+                          gefs.setup.recent$validtime[24], by = '12 hours')
+p <- ggplot(gefs.setup.recent, aes(x = validtime)) + 
+    geom_hline(aes(yintercept = 0), linetype = 'dashed') + 
+    geom_vline(aes(xintercept = as.numeric(gefs.setup.recent$validtime[1])), 
+               linetype = 'dashed') +
+    geom_line(data = gefs.setup.1, aes(y = max), alpha = 0.5) + 
+    # geom_line(data = gefs.setup.1, aes(y = avg), alpha = 0.5) + 
+    geom_line(data = gefs.setup.1, aes(y = min), alpha = 0.5) + 
+    geom_ribbon(data = gefs.setup.1, aes(ymin = min, ymax = max), 
+                alpha = 0.125) + 
+    geom_line(data = gefs.setup.2, aes(y = max), alpha = 0.5) + 
+    # geom_line(data = gefs.setup.2, aes(y = avg), alpha = 0.5) + 
+    geom_line(data = gefs.setup.2, aes(y = min), alpha = 0.5) + 
+    geom_ribbon(data = gefs.setup.2, aes(ymin = min, ymax = max), 
+                alpha = 0.125) + 
+    geom_line(data = gefs.setup.3, aes(y = max), alpha = 0.5) + 
+    # geom_line(data = gefs.setup.3, aes(y = avg), alpha = 0.5) + 
+    geom_line(data = gefs.setup.3, aes(y = min), alpha = 0.5) + 
+    geom_ribbon(data = gefs.setup.3, aes(ymin = min, ymax = max), 
+                alpha = 0.125) + 
+    geom_line(data = gefs.setup.recent.melt, 
+              mapping = aes(x = validtime, y = value, color = variable)) + 
     geom_line(aes(y = gec00.raw, color = 'Ensemble Members')) + 
-    geom_line(aes(y = med.raw, color = 'Ensemble Median'), size = 1.5) + 
-    geom_line(aes(y = avg.raw, color = 'Ensemble Mean'), size = 1.5) + 
-    geom_line(aes(y = max.raw)) + geom_line(aes(y = min.raw)) + 
-    geom_ribbon(aes(ymin = min.raw, ymax = max.raw, fill = 'Ensemble Spread'), 
+    geom_line(aes(y = med, color = 'Ensemble Median'), size = 1.5) + 
+    geom_line(aes(y = avg, color = 'Ensemble Mean'), size = 1.5) + 
+    geom_line(aes(y = max)) + geom_line(aes(y = min)) + 
+    geom_ribbon(aes(ymin = min, ymax = max, fill = 'Ensemble Spread'), 
                 alpha = 0.25) + 
+    geom_point(data = asos.setup, mapping = aes(x = roundvalid, y = setup), 
+              size = 3, fill = 'orange', shape = 21) +
     scale_color_manual(breaks = c('Ensemble Median', 'Ensemble Mean', 
                                   'Ensemble Members'), 
                        values = c('red', 'blue', 'grey', rep('grey', 21))) +
     scale_fill_manual(breaks = c('Ensemble Spread'), 
                       values = c('black')) +
-    geom_hline(aes(yintercept = 0), linetype = 'dashed') + theme_light() + 
-    xlab('') + ylab('IRL Setup (cm)') + 
-    scale_x_datetime(breaks = df.test$validtime[seq(4, 28, by = 4)], 
-                     date_labels = '%b %d\n %H UTC', 
-                     limits = c(df.test$validtime[1], NA)) +
+    theme_light() + xlab('') + ylab('IRL Setup (cm)') + 
+    scale_x_datetime(breaks = date.breaks,
+                     date_labels = '%b %d\n %H UTC',
+                     limits = c(date.breaks[1], NA)) +
     theme(legend.position="bottom", legend.title = element_blank())
 print(p)
--- R/calculate_irl_setup.R
+++ R/calculate_irl_setup.R
@@ -10,20 +10,26 @@
 
 # libraries ---------------------------------------------------------------
 
-library(lubridate) # nice date handling
 library(rmarkdown) # rendering index.Rmd at the end
+library(riem) # access to ASOS data through iowa state
+library(lubridate) # awesome date handling
+library(xts) # also awesome date handling
+library(WindVerification) # wind data handling
 
 # functions ---------------------------------------------------------------
 
 # calculates the 12-hour wind run given a series of u and v forecasts, returns
 # the u and v wind run components
-getWindRun <- function(times, us, vs) {
+getWindRun <- function(times, us, vs, type = 'model') {
     # create a dataframe given input variables
     df <- data.frame(time = times, u = us, v = vs)
     
     # we can't calculate the 12 hour wind run until we have 12 hours of data!
     # So don't start calculating until this starting point
     windRun.start <- df$time[1] + hours(9)
+    
+    # if we are doing KMLB obs
+    if (type == 'asos') windRun.start <- df$time[1] + hours(11)
     
     df.length <- length(df$time)
     
@@ -56,22 +62,28 @@ deg2rad <- function(degrees) {
     return((degrees * pi) / 180)
 }
 
-# get the wind speed and direction from u and v components
-getwspdwdir <- function(u, v) {
+# calculate the setup 
+getSetup <- function(times, us, vs, type = 'model') {
     
-    # If either u or v are missing, we cannot do the calculation!
-    if (anyNA(c(u, v))) {return(c(NA, NA))}
+    # calculate 12 hour wind run
+    windRun <- getWindRun(times, us, vs, type)
     
-    # Calculate wind speed and wind direction
-    wspd <- sqrt(u^2 + v^2)
-    wdir <- atan2(-1 * u, -1 * v) * 57.2957795131 # 57.2957795131 is 180 / pi
+    # get wind speed and wind direction for these wind runs
+    windRun.spd.dir <- mapply(getwspdwdir, windRun$u, windRun$v)
     
-    # atan2 goes from -pi to pi, so you need to add 360 if negative
-    if (wdir < 0) {wdir <- wdir + 360}
-    if (round(wdir) >= 360) {wdir <- 0}
+    # calculate u1 and u2, the irl-oriented wind components for the northern 
+    # and southern irl, respectively
+    u1 <- unlist(windRun.spd.dir[1,]) * 
+        cos(sapply(unlist(windRun.spd.dir[2,]) + 10, deg2rad))
+    u2 <- unlist(windRun.spd.dir[1,]) * 
+        cos(sapply(unlist(windRun.spd.dir[2,]) + 26, deg2rad))
     
-    # return wind speed first, then wind direction rounded to nearest integer
-    return(list(wspd = wspd, wdir = round(wdir, 0)))
+    # now calculate the lagoon relative wind
+    u.r <- -1 * (u1 * 28 + u2 * 70) / (28 + 70)
+    # u.r <- (u1 * 28 + u2 * 70) / (28 + 70)
+    
+    # return the setup given u.r
+    return(0.728 * sign(u.r) * abs(u.r)^1.5 - 3)
 }
 
 # global variables --------------------------------------------------------
@@ -84,48 +96,90 @@ ens.mems <- c('gec00', 'gep01', 'gep02', 'gep03', 'gep04', 'gep05', 'gep06',
               'gep07', 'gep08', 'gep09', 'gep10', 'gep11', 'gep12', 'gep13', 
               'gep14', 'gep15', 'gep16', 'gep17', 'gep18', 'gep19', 'gep20')
 
-# most recent data (yesterday)
-date <- format(Sys.Date() - days(1), '%Y%m%d')
+# # most recent data (yesterday)
+# date <- format(Sys.Date() - days(1), '%Y%m%d')
 
 # data --------------------------------------------------------------------
 
-# in v0.1, all we need is yesterday's data
-df.recent <- read.csv(paste(data.path, '/gefs_', date, '.csv', sep = ''), 
-                      header = TRUE, stringsAsFactors = FALSE)
-df.recent$runtime <- as.POSIXct(df.recent$runtime, tz = 'GMT')
-df.recent$validtime <- as.POSIXct(df.recent$validtime, tz = 'GMT')
+# open df.all, we will need a few days of gefs forecasts
+df.all <- read.csv(paste(data.path, '/gefs_all.csv', sep = ''), 
+                         header = TRUE, stringsAsFactors = FALSE)
+df.all$runtime <- as.POSIXct(df.all$runtime, tz = 'UTC')
+df.all$validtime <- as.POSIXct(df.all$validtime, tz = 'UTC')
 
-# dataframe to store setup information
-df.setup <- data.frame(validtime = df.recent$validtime)
+# we will only calculate setup for the last 4 gefs runs
+gefs.runs <- tail(unique(df.all$runtime), 4)
+
+# get data frames for only these four runs, and neglect kmlb.u & kmlb.v columns
+df.gefs.1 <- df.all[df.all$runtime == gefs.runs[1],-c(46, 47)]
+df.gefs.2 <- df.all[df.all$runtime == gefs.runs[2],-c(46, 47)]
+df.gefs.3 <- df.all[df.all$runtime == gefs.runs[3],-c(46, 47)]
+df.gefs.recent <- df.all[df.all$runtime == gefs.runs[4],-c(46, 47)]
+
+# clear up some memory
+rm(df.all)
+
+# data frames to store setup information
+gefs.setup.1 <- data.frame(validtime = df.gefs.1$validtime)
+gefs.setup.2 <- data.frame(validtime = df.gefs.2$validtime)
+gefs.setup.3 <- data.frame(validtime = df.gefs.3$validtime)
+gefs.setup.recent <- data.frame(validtime = df.gefs.recent$validtime)
 
 # loop through all the ensemble members and calculate the wind runs
 for (ens.mem in ens.mems) {
-# for (ens.mem in 'gec00') { # for testing purposes
-    
-    # calculate 12 hour wind run given ensemble us and vs
-    mem.windruns <- getWindRun(times = df.setup$validtime, 
-                               df.recent[[paste(ens.mem, 'u', sep = '.')]], 
-                               df.recent[[paste(ens.mem, 'v', sep = '.')]])
-    
-    # get wind speed and wind direction for these wind runs
-    mem.wspd.wdir <- mapply(getwspdwdir, mem.windruns$u, mem.windruns$v)
-    
-    # calculate u1 and u2, the irl-oriented wind components for the northern and
-    # southern irl, respectively
-    u1 <- unlist(mem.wspd.wdir[1,]) * cos(sapply(unlist(mem.wspd.wdir[2,]) + 10, 
-                                                 deg2rad))
-    u2 <- unlist(mem.wspd.wdir[1,]) * cos(sapply(unlist(mem.wspd.wdir[2,]) + 26, 
-                                                 deg2rad))
-    
-    # now calculate the lagoon relative wind
-    u.r <- -1 * (u1 * 28 + u2 * 70) / (28 + 70)
-    # u.r <- (u1 * 28 + u2 * 70) / (28 + 70)
-    
-    # calculate setup and add it to df.setup
-    df.setup[[paste(ens.mem, 'raw', sep = '.')]] <- 0.728 * sign(u.r) * 
-        abs(u.r)^1.5 - 3
+
+    # calculate setup for each gefs run and add it to the respective setup
+    # data frame
+    gefs.setup.1[[paste(ens.mem, 'raw', sep = '.')]] <- 
+        getSetup(times = df.gefs.1$validtime, 
+                 df.gefs.1[[paste(ens.mem, 'u', sep = '.')]], 
+                 df.gefs.1[[paste(ens.mem, 'v', sep = '.')]])
+    gefs.setup.2[[paste(ens.mem, 'raw', sep = '.')]] <- 
+        getSetup(times = df.gefs.2$validtime, 
+                 df.gefs.2[[paste(ens.mem, 'u', sep = '.')]], 
+                 df.gefs.2[[paste(ens.mem, 'v', sep = '.')]])
+    gefs.setup.3[[paste(ens.mem, 'raw', sep = '.')]] <- 
+        getSetup(times = df.gefs.3$validtime, 
+                 df.gefs.3[[paste(ens.mem, 'u', sep = '.')]], 
+                 df.gefs.3[[paste(ens.mem, 'v', sep = '.')]])
+    gefs.setup.recent[[paste(ens.mem, 'raw', sep = '.')]] <- 
+        getSetup(times = df.gefs.recent$validtime, 
+                 df.gefs.recent[[paste(ens.mem, 'u', sep = '.')]], 
+                 df.gefs.recent[[paste(ens.mem, 'v', sep = '.')]])
 }
-save(df.setup, file = 'data/df_setup.RData')
+# clear up some memory
+rm(df.gefs.1, df.gefs.2, df.gefs.3, df.gefs.recent)
+
+# get hourly KMLB ASOS data for the last few days to plot
+df.kmlb <- riem_measures(station = "MLB", 
+                         date_start = format(Sys.Date() - days(4), '%Y-%m-%d'), 
+                         date_end = format(Sys.Date() + days(1), '%Y-%m-%d'))
+
+# Only keep the hourly updates, which happen to be the only observations with
+# MSLP
+df.kmlb <- df.kmlb[!is.na(df.kmlb$mslp),]
+
+# round valid times to nearest quarter hour, n is in seconds
+df.kmlb$roundvalid <- as.POSIXct(align.time(df.kmlb$valid, n=60*15))
+df.kmlb$roundvalid <- as.POSIXct(format(df.kmlb$roundvalid, 
+                                        '%Y-%m-%d %H:%M:%S'), tz = 'UTC')
+
+# convert wind speed from knots to m/s
+df.kmlb$wspd <- convertunits(df.kmlb$sknt, inunits = 'knots', outunits = 'm/s')
+
+# get u and v
+uv <- mapply(getuv, df.kmlb$wspd, df.kmlb$drct)
+df.kmlb$u <- uv[1,]
+df.kmlb$v <- uv[2,]
+
+# calculate KMLB ASOS setup
+kmlb.setup <- getSetup(df.kmlb$roundvalid, df.kmlb$u, df.kmlb$v, 
+                       type = 'asos')
+
+# create a dataframe with just this information
+asos.setup <- data.frame(roundvalid = df.kmlb$roundvalid, setup = kmlb.setup)
+save(gefs.setup.1, gefs.setup.2, gefs.setup.3, gefs.setup.recent, asos.setup, 
+     file = 'data/setup.RData')
 rmarkdown::render('R/index.Rmd', output_dir = '~/Dropbox/IRLSetup/docs/')
 # save df.run to disk
 # write.csv(df.run, file = paste(data.path, '/', 'gefs_', date, '.csv', sep = ''), 
